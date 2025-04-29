25d902a0d2847ead1e7874954d2f9066a200fd55
Farrah Roa <20200019173@my.xu.edu.ph>
2025-04-29 19:48:49 +0800

    2nd

--- Backend/Booking_Backend/Room_Booking/views.py
+++ Backend/Booking_Backend/Room_Booking/views.py
@@ -1,6 +1,72 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Room
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from .models import Room, OccupiedDate, User
+from .serializers import RoomSerializer, OccupiedDateSerializer, UserSerializer
 
-class RoomList(generics.ListAPIView):
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response ({
+        'rooms': reverse('room-list', request=request, format=format)
+    })
+
+class RoomList(generics.ListCreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class RoomDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
     
+class OccupiedDatesList(generics.ListCreateAPIView):
+    queryset = OccupiedDate.objects.all()
+    serializer_class = OccupiedDateSerializer
+    
+    def get_queryset(self):
+        user = self.request.user
+        if not user.is_superuser and not user.is_staff:
+            return OccupiedDate.objects.filter(user=user) 
+        
+        return super().get_queryset()
+
+
+class OccupiedDatesDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = OccupiedDate.objects.all()
+    serializer_class = OccupiedDateSerializer
+    
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_admin or user.is_superuser:
+            return User.objects.all()
+        else:
+            return User.objects.filter(id=user.id)
+    
+    
+    
+class UserDetail(generics.RetrieveAPIView):
+    queryset = OccupiedDate.objects.all()
+    serializer_class = OccupiedDateSerializer
+    
+    def get_object(self):
+        user = self.request.user
+        obj = super.get_object()
+        
+        if obj == user or user.is_admin or user.is_superuser:
+            return obj
+        else:
+            pass
+
+class Register(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+    def perform_create(self, serializer):
+        return super().perform_create(serializer)
